2fa0b58db78af1c38a47492a8715f0504bc83017
kains123 <kains0417@naver.com>
2020-12-26 20:20:33 +0900

    Update Test

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,26 +1,47 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { 
+    Body, 
+    Controller, 
+    Delete, 
+    Get, 
+    Param, 
+    Patch, 
+    Post, 
+    Query 
+} from '@nestjs/common';
+import { CreateContextOptions } from 'vm';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService} from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
     @Get()
-    getAll(){
-        return "This will return all movies";
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
+    }
+    @Get('search')
+    search(@Query("year") searchingYear: string){
+        return `We are searching for a movie with a title : ${searchingYear}`;
     }
     @Get("/:id")
-    getOne(@Param('id') movieId: string){
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param('id') movieId: number): Movie {
+        return this.moviesService.getOne(movieId);
     }
     @Post()
-    create(@Body() movieData) {
-        console.log(movieData);
-        return 'This will create a movie';
+    create(@Body() movieData: CreateMovieDto) {
+        return this.moviesService.create(movieData);
     }
     @Delete("/:id")
-    remove(@Param('id') movieId:string){
-        return `This will delete a movie with the id: ${movieId}`;
+    remove(@Param('id') movieId:number){
+        return this.moviesService.deleteOne(movieId);
     }
     @Patch('/:id')
-    patch(@Param('id') movieId: string){
-        return `This will delete a movie with the id: ${movieId}`;
+    patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto){
+        return {
+            updateMovie: movieId,
+            ...updateData,
+        }
     }
 }
